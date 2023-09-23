42da14ecd33e1c719a498a7d6e6e5e57c0da7dff
julieprunaret <j.prunaret@gmail.com>
2023-09-23 11:49:51 +0200

    add - open and close menu function

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,14 +4,29 @@ import { useState } from 'react';
 function Cart() {
     const monsteraPrice = 8;
     const [Cart, setCart] = useState(0);
+    const [open, setOpen] = useState(false);
 
-    return (
-        <div className='lmj-cart'>
-            <h2>Panier</h2>
-            <p>Monstera {monsteraPrice} €</p>
-            <p>Total : {monsteraPrice * Cart}€</p>
-            <button onClick={() => setCart(Cart + 1)}>Ajouter</button>
+    //my soluce
+    // const isMenuOpen = () => {
+    //     setOpen(current => !current)
+    // }
+
+    return open ? (
+        <div>
+            {/* <button onClick={isMenuOpen}>
+                {open ? 'Fermer' : 'Ouvrir'}
+            </button> */}
+            {/* <div className={`lmj-cart ${open && 'lmj-cart_open'}`}> */}
+            <div className="lmj-cart">
+            <button onClick={() => setOpen(false)}>Fermer</button>
+                <h2>Panier</h2>
+                <p>Monstera {monsteraPrice} €</p>
+                <p>Total : {monsteraPrice * Cart}€</p>
+                <button onClick={() => setCart(Cart + 1)}>Ajouter</button>
+            </div>
         </div>
+    ) : (
+        <button onClick={() => setOpen(true)}>Ouvrir</button>
     )
 }
 
