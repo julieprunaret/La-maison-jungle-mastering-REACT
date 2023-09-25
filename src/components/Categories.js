f78a24bdeefcc8007d6244a72f75f03e64019c7c
julieprunaret <j.prunaret@gmail.com>
2023-09-25 17:17:17 +0200

    correction -  categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,35 +1,59 @@
-import { plantList } from '../datas/plantList';
+// import { plantList } from '../datas/plantList';
 
 
-function Categories ({newPlantList, setNewPlantList}) {
+// function Categories ({newPlantList, setNewPlantList}) {
 
-    console.log("voici plantList", plantList)
-    // we get the list of plants and push the values in a tab
-    const categories = plantList.reduce((acc, plant) => 
-    // if the category isn't already in the array, we push it inside
-    acc.includes(plant.category)? acc : acc.concat(plant.category), []);
+//     // we get the list of plants and push the values in a tab
+//     const categories = plantList.reduce((acc, plant) => 
+//     // if the category isn't already in the array, we push it inside
+//     acc.includes(plant.category)? acc : acc.concat(plant.category), []);
 
-    let initValue = newPlantList === plantList ? "B" : null;
+//     let initValue = newPlantList === plantList ? "B" : null;
 
-    function addValue (e) {
-        setNewPlantList(newPlantList=plantList.filter((plant) => plant.category === e.target.value))
-        initValue = e.target.value;
-    }
+//     function addValue (e) {
+//         setNewPlantList(newPlantList=plantList.filter((plant) => plant.category === e.target.value))
+//         initValue = e.target.value;
+//     }
 
-    return (
-        <div>
-            <select onChange={(e) => {addValue(e)}} value="B">
-                <option  value={initValue} selected disabled hidden>Choisissez une catégorie</option>
-                {
-                        categories.map((category) => (
-                        // because the category is unique, we can use the name as key
-                        <option key={category} value={category}>{category}</option>
-                    ))
-                }
-            </select>
-            { newPlantList !== plantList && <button onClick={() => {setNewPlantList(plantList)}}>Réinitialiser</button> } 
-        </div>
-    )
+//     return (
+//         <div>
+//             <select onChange={(e) => {addValue(e)}} value="B">
+//                 <option  value={initValue} selected disabled hidden>Choisissez une catégorie</option>
+//                 {
+//                         categories.map((category) => (
+//                         // because the category is unique, we can use the name as key
+//                         <option key={category} value={category}>{category}</option>
+//                     ))
+//                 }
+//             </select>
+//             { newPlantList !== plantList && <button onClick={() => {setNewPlantList(plantList)}}>Réinitialiser</button> } 
+//         </div>
+//     )
+// }
+
+// export default Categories
+
+import '../styles/Categories.css'
+
+function Categories({ setActiveCategory, categories, activeCategory }) {
+	return (
+		<div className='lmj-categories'>
+			<select
+                //if no category is active, there is no value ('')
+				value={activeCategory}
+				onChange={(e) => setActiveCategory(e.target.value)}
+				className='lmj-categories-select'
+			>
+				<option value=''>Filter par catégorie</option>
+				{categories.map((category) => (
+					<option key={category} value={category}>
+						{category}
+					</option>
+				))}
+			</select>
+			<button onClick={() => setActiveCategory('')}>Réinitialiser</button>
+		</div>
+	)
 }
 
 export default Categories
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,8 +5,16 @@ import PlantItem from './PlantItem';
 import React, { useState } from 'react';
 
 const ShoppingList = ({cart, setCart}) => {
-    const [newPlantList, setNewPlantList] = useState(plantList)
-    console.log("newPlantList :" ,newPlantList);
+    // const [newPlantList, setNewPlantList] = useState(plantList)
+    // console.log("newPlantList :" ,newPlantList);
+
+    // we create the state
+    const [activeCategory, setActiveCategory] = useState('')
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
     function addToCart(name, price) {
         const currentPlantAdded = cart.find((plant)=> plant.name === name)
@@ -25,11 +33,18 @@ const ShoppingList = ({cart, setCart}) => {
 
     return (
         <div>
-            <Categories setNewPlantList={setNewPlantList} newPlantList={newPlantList}/>
+            {/* <Categories setNewPlantList={setNewPlantList} newPlantList={newPlantList}/> */}
+            <Categories
+				categories={categories} //all the categories
+				setActiveCategory={setActiveCategory} // fonction to set the category state
+				activeCategory={activeCategory} // category state (immutable)
+			/>
 
             <ul className='lmj-plant-list'>
                         {
-                            newPlantList.map(({ id, cover, name, water, light, isSpecialOffer, price }) => (
+                            plantList.map(({ id, cover, name, water, light, isSpecialOffer, price, category }) => 
+                            //if a category is selected, we show the plant if the plant's category is the same. If no category is selected, we show all the plants
+                            !activeCategory || activeCategory === category ? (
                                 <div key={id}>
                                     <PlantItem 
                                         name={name} 
@@ -41,7 +56,8 @@ const ShoppingList = ({cart, setCart}) => {
                                     />
                                     <button onClick={() => addToCart(name, price)}>Ajouter</button>
                                 </div>
-                        ))}
+                        ) : null
+                        )}
             </ul>
         </div>
     )
